1bce0800fdf6261b321b91212d3d1adfc96c653b
freshbell <freshbell95@gmail.com>
2021-11-18 18:56:53 +0900

    add

--- app.py
+++ app.py
@@ -1,7 +1,66 @@
-from asyncio
-from socketio
-from flask_socketio import send, emit
-import threading
+from threading import Lock
+from flask import Flask, render_template, session, request, copy_current_request_context
+from flask_socketio import SocketIO, emit, join_room, leave_room, close_room, rooms, disconnect
+from engineio.payload import Payload
 import json
 
- 
+Payload.max_decode_packets = 101
+async_mode = None
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+socketio = SocketIO(app, async_mode=async_mode)
+thread = None
+thread_lock = Lock()
+
+clients = {}
+
+def background_thread():
+    MOVE_JSON = {
+        'DATA_TYPE':'moveCommand',
+        'AGV_NO':'temp',
+        'ACTION':'1',
+        'BLOCKS':[
+            '00010002',
+            '00020003',
+        ]
+    }
+
+    while True:
+        socketio.sleep(3)
+        for AGV in clients.keys():
+            MOVE_JSON['AGV_NO'] = AGV
+            socketio.emit('move',json.dumps(MOVE_JSON), room=clients[AGV])
+
+@socketio.on('connect')
+def connect():
+    global thread
+
+    clients[request.headers['AGV_NO']] = request.sid
+
+    REPORT_REQUEST = {
+        'DATA_TYPE':'reportRqst',
+        'AGV_NO':request.headers['AGV_NO'],
+    }
+
+    socketio.emit('state',json.dumps(REPORT_REQUEST), room=request.sid)
+
+    with thread_lock:
+        if thread is None:
+            thread = socketio.start_background_task(background_thread)
+
+@socketio.on('disconnect')
+def disconnect():
+    print("disconnected")
+    del clients[request.headers['AGV_NO']]
+
+@socketio.on('state')
+def state(data):
+    print(str(data))
+
+@socketio.on('alarm')
+def alarm(data):
+    print(str(data))
+
+if __name__=="__main__":
+    socketio.run(app)
---
+++ client.py
@@ -0,0 +1,81 @@
+import asyncio
+from os import name
+import socketio
+from flask_socketio import send, emit
+import threading
+import json
+
+test_json = {
+    'DATA_TYPE':'report',
+    'AGV_NO':'AGV00001',
+    'LOCATION':'00010002',
+    'STATE':'1',
+    'MODE':'1',
+    'DIRECTION':'0',
+    'MAX_VELOCITY':'2.5',
+    'TILT_MAX_ANGLE':'20',
+    'BELT_MAX_SPEED':'1.5',
+    'COMMAND_WAIT_TIME':'10',
+    'MIN_VOLTAGE':'15.6',
+    'BATTERY_LVL':'30',
+    'AGV_FIRMWARE_VERSION':'1.01'
+}
+
+alarm_json = {
+    'DATA_TYPE':'alarm',
+    'AGV_NO':'AGV0001',
+    'ALARMS':[
+        {
+            'ALARM_CD':'11',
+            'ALARM_STATUS':'1',
+            'OCCUR_DT':'20210817 13:44:22',
+        },
+        {
+            'ALARM_CD':'12',
+            'ALARM_STATUS':'0',
+            'OCCUR_DT':'20210817 13:44:22',
+            'END_DT':'20210817 13:46:55',
+        }
+    ]
+}
+
+sio = socketio.AsyncClient()
+
+@sio.event
+async def connect():
+    # connect되면 알람 발생
+    await send_alarm()
+
+async def send_alarm():
+    await sio.emit('alarm',json.dumps(alarm_json))
+
+# AGV 상태요청 receive
+@sio.on('state')
+async def state(data):
+    json_data = json.loads(data)
+
+    # 상태보고 전송 Thread 시작
+    if json_data['DATA_TYPE'] == 'reportRqst':
+        sio.start_background_task(send_state)
+
+# AGV 상태보고 전송
+async def send_state():
+    while True:
+        await sio.emit('state',json.dumps(test_json))
+        await sio.sleep(3)
+
+# AGV 이동 명령 receive
+@sio.on('move')
+async def move_avg(data):
+    print(str(data))
+
+@sio.event()
+async def disconnect():
+    print('disconnected from server')
+
+async def main():
+    await sio.connect('http://127.0.0.1:5000',headers={'AGV_NO':'AGV00001'})
+    await sio.wait() 
+
+if __name__ == '__main__':
+    asyncio.run(main()) 
