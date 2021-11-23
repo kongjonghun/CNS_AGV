9fd3284ea57a28301460449ae492aff0069c17fe
freshbell <freshbell95@gmail.com>
2021-11-23 18:23:51 +0900

    server

--- app.py
+++ app.py
@@ -96,5 +96,5 @@ def alarm(data):
     print(str(data))
 #test
 if __name__=="__main__":
-    #socketio.run(app,host='0.0.0.0')
-    socketio.run(app)
+    socketio.run(app,host='0.0.0.0')
+    #socketio.run(app)
